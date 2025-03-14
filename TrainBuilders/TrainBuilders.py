43978cbf78de2c95e296aad9e7c1996519279ddc
sakitibi <sakitibi10nin@icloud.com>
2025-03-14 12:59:55 +0900

    Update TrainBuilders.py

--- TrainBuilders/TrainBuilders.py
+++ TrainBuilders/TrainBuilders.py
@@ -20,7 +20,7 @@ with st.form(key='12ninaccounts'):
         mail = st.text_input('EMail')
         password = st.text_input('パスワード')
         
-        submit_btn = ('ログイン')
+        submit_btn = st.form_submit_button('ログイン')
         cancel_btn = st.form_submit_button('キャンセル')
         if submit_btn:
                 webbrowser.open_new_tab('https://sakitibi-com9.webnode.jp/api/trainbuilders/login/24ac77a0-013e-45ae-97d1-b6cc59fb958a/')
